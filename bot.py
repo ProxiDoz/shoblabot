38663c9f78c2ffda625cdde8f123b3ffb0b6d711
Nosov Alexey <proxidoz@gmail.com>
2020-04-01 20:01:29 +0300

    Try

--- bot.py
+++ bot.py
@@ -1392,8 +1392,7 @@ def coronasticker():
         bot.send_sticker(secret.tg_chat_id, 'CAACAgIAAxkBAAJ7k16EtnmSM4Wzy5BjSryqZymDondiAAK3oAIAAZJlSgvdtGD02Ww35xgE')  # Мир
         bot.send_sticker(secret.tg_chat_id, 'CAACAgIAAxkBAAJ7jV6EthT3WNF6k-BA1cyMC4A395VyAAK4oAIAAZJlSgtYkgABa0Y4cncYBA')  # Россия
     except Exception as e:
-        bot.send_message(secret.apple_id,
-                         'Ошибка в функции отправки поздравления в Шоблу coronasticker():\n\n' + str(e))
+        bot.send_message(secret.apple_id, 'Ошибка в функции отправки поздравления в Шоблу coronasticker():\n\n' + str(e))
 
 try:
     coronasticker()
