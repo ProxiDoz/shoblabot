bf765c61d38c3ebdb4f5d9220d2f29ff00813ade
anashkinaa <anashkin93@gmail.com>
2023-03-30 11:47:39 +0300

    comment prints

--- helpers/find_words.py
+++ helpers/find_words.py
@@ -3,9 +3,9 @@ import re
 def wordInMessage(message = '', trigger_words_list = []):
   pattern = r'\W+'
   message_as_list = re.split(pattern, message)
-  print(trigger_words_list)
+  # print(trigger_words_list)
   for word in trigger_words_list:
-    print(word)
+    # print(word)
     if word.lower() in message_as_list:
       return True
   return False
