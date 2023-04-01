4dfbb2cdf379ee2f8262008778783b762845623f
anashkinaa <anashkin93@gmail.com>
2023-04-01 12:09:17 +0300

    kirov upd

--- helpers/find_words.py
+++ helpers/find_words.py
@@ -2,7 +2,7 @@ import re
 
 def wordInMessage(message = '', trigger_words_list = []):
   pattern = r'\W+'
-  message_as_list = re.split(pattern, message)
+  message_as_list = re.split(pattern, message.lower())
   # print(trigger_words_list)
   for word in trigger_words_list:
     # print(word)
