22a11f1e60c9e098362b284259513405919a1991
anashkinaa <anashkin93@gmail.com>
2023-05-11 10:43:27 +0300

    discount func upd

--- bot.py
+++ bot.py
@@ -79,8 +79,8 @@ def send_discount(message):
                 i += 2
             # keyboard_start.add(constants.discounts, constants.channel)
             bot.send_message(message.chat.id, constants.buttons[2][0], reply_markup=keyboard_start, parse_mode='MarkdownV2')
-            # if message.from_user.is_premium and random.random() < 0.3:
-            #     bot.send_message(message.chat.id, '🤗 Экономить всегда полезно, ||пусечка|| премиумная', parse_mode='MarkdownV2')
+            if message.from_user.is_premium and random.random() < 0.3:
+                bot.send_message(message.chat.id, '🤗 Экономить всегда полезно, ||пусечка|| премиумная', parse_mode='MarkdownV2')
             update_activity('discount')
     except Exception as e:
         log('{0}\nТекст ошибки: {1}'.format(constants.errors[8], e))
