b5ca4ed8831bcaa24e58c4d3fed8265d0cafe842
anashkinaa <anashkin93@gmail.com>
2023-08-12 22:08:21 +0300

    add media file id parcing

--- bot.py
+++ bot.py
@@ -445,9 +445,19 @@ def poll_results(poll):
 @bot.message_handler(content_types=['photo'])
 def send_photo(message):
     try:
-        bot.send_message(secret.apple_id, message.photo[2].file_id)
+        if message.from_user.id == secret.apple_id:
+            bot.send_message(secret.apple_id, message.photo[2].file_id)
     except Exception as e:
         bot.send_message(secret.apple_id, '❌ Ошибка в функции send_photo:\n*Сообщение: *{0}\n*Текст ошибки:*\n{1}'.format(message, e), parse_mode='MarkdownV2')
+
+
+@bot.message_handler(content_types=['audio'])
+def send_audio(message):
+    try:
+        if message.from_user.id == secret.apple_id:
+            bot.send_message(secret.apple_id, message.audio.file_id)
+    except Exception as e:
+        bot.send_message(secret.apple_id, '❌ Ошибка в функции send_audio:\n*Сообщение: *{0}\n*Текст ошибки:*\n{1}'.format(message, e), parse_mode='MarkdownV2')
 
 
 # # # # # # Обработка реплаев # # # # # #
