7c4a229f630be941356cd5ee5838e73afaa9adf7
Tar4s <kalashnikof@inbox.ru>
2023-08-12 16:43:40 +0400

    Feature/usd cbr (#22)
    * Create cbr.py
    
    function with cbrf usd rate parsing
    
    * add cbr handle
    
    * Update cbr.py
    
    * Update bot.py
    
    * Update bot.py

---
+++ helpers/cbr.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.cbr.ru/currency_base/daily/'
+
+def getUSD():
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, 'html.parser')
+  
+  table = soup.find('table', {'class': 'data'})
+  
+  for row in table.find_all('tr'):
+      tds = row.find_all('td')
+      
+      if len(tds) == 5 and tds[1].text.strip() == "USD":
+          return tds[-1].text
+
+  return "Жеппа твоя доллар"
