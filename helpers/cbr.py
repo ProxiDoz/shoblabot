14cfbe02925ad7240c298d43700981ed63cc9db3
anashkinaa <anashkin93@gmail.com>
2023-11-15 15:30:56 +0300

    txt upd
    change of func name

--- helpers/cbr.py
+++ helpers/cbr.py
@@ -3,15 +3,13 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.cbr.ru/currency_base/daily/'
 
-def getUSD(currency):
-  response = requests.get(url)
-  soup = BeautifulSoup(response.content, 'html.parser')
 
-  table = soup.find('table', {'class': 'data'})
-
-  for row in table.find_all('tr'):
-      tds = row.find_all('td')
-      if len(tds) == 5 and tds[1].text.strip() == currency:
-          return tds[-1].text
-
-  return "Жеппа твоя доллар"
+def get_usd(currency):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'class': 'data'})
+    for row in table.find_all('tr'):
+        tds = row.find_all('td')
+        if len(tds) == 5 and tds[1].text.strip() == currency:
+            return tds[-1].text
+    return "Жеппа твоя доллар"
