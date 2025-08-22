58cc870aecb2d68640985aee9b75292d7cc37039
anashkinaa <anashkin93@gmail.com>
2025-08-22 10:48:02 +0300

    Update currency ids
    cbr blyat naebdulina added some shit

--- helpers/cbr.py
+++ helpers/cbr.py
@@ -9,10 +9,10 @@ def get_exchange_rates():
     try:
         response = requests.get(cbr_url)  # Парсим страницу с xml содержимым
         tree = ElementTree.fromstring(response.content)  # Конвертируем из строки в дерево
-        usd = '%.2f' % float(tree[13][5].text.replace(',', '.'))
-        eur = '%.2f' % float(tree[14][5].text.replace(',', '.'))
-        lar = '%.2f' % float(tree[10][5].text.replace(',', '.'))
-        ten = '%.2f' % float(tree[18][4].text.replace(',', '.'))
+        usd = '%.2f' % float(tree[16][5].text.replace(',', '.'))
+        eur = '%.2f' % float(tree[17][5].text.replace(',', '.'))
+        lar = '%.2f' % float(tree[13][5].text.replace(',', '.'))
+        ten = '%.2f' % float(tree[21][4].text.replace(',', '.'))
         date = tree.attrib['Date']
         return usd, eur, lar, ten, date
     except Exception as get_exchange_rates_error:
